24334ba8865ed3e162f620e036bd95a5fb41720f
Manuel Del Basso <manuel.delbasso@gmail.com>
2017-03-14 16:47:05 +0100

    Initial commit after excluding project from workspace repository

---
+++ doxygen/html/navtreedata.js
@@ -0,0 +1,40 @@
+var NAVTREE =
+[
+  [ "XY-GalvoScanner", "index.html", [
+    [ "Project: XY-Galvo Scanner", "index.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Structure Index", "classes.html", null ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Variables", "functions_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"a00002.html",
+"a00005.html#gga014461f4d497805581f99288e88d6e13a3c7c39c7c0dced95dd0280153fba0218",
+"a00049.html#a9dd03edf61dfb4c2945d6e3344463605",
+"a00081.html#gaf957d16485c2d89b9585649bb492549e",
+"a00109.html#gacb7d404e3eeefe13a4cd5ebe539d2926",
+"a00143.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
